411599e352167059b97f19d2ecbb4630a44a8e63
saho <88282070+sahosandeepa@users.noreply.github.com>
2021-12-08 22:37:04 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -6,6 +6,19 @@
 
 int main() {
   
+   float mark1=0,mark2=0;
+   float sum,avg;
+   
+   printf("Enter your first subject mark here: ");scanf("%f", &mark1);
+   printf("Enter your second sebject mark here: ");scanf("%f", &mark2);
+   
+   sum=0,avg=0;
+   
+   sum = mark1 + mark2;
+   
+   printf("\nAverage of two subjects: %.2f", sum/2);
+   
+   puts("");
   return 0;
 }
 
